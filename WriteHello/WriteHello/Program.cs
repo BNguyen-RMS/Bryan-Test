94d55cf6cc44de72f71df087bd1851ecd3d25786
Bryan Nguyen <Bryan.Nguyen@rms.com>
2018-10-03 15:58:42 -0700

    Update Program.cs
    Test 2

--- WriteHello/WriteHello/Program.cs
+++ WriteHello/WriteHello/Program.cs
@@ -12,7 +12,7 @@ namespace WriteHello
         static void Main()
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World! from develop branch");
+            Console.WriteLine("Hello World! from the develop branch");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
