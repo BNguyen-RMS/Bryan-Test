67de14215f0473829f82670e68916ab5588cc1b6
Bryan Nguyen <Bryan.Nguyen@rms.com>
2018-10-04 10:27:06 -0700

    Update Program.cs
    Oct 4, 2018 - 10:26AM

--- WriteHello/WriteHello/Program.cs
+++ WriteHello/WriteHello/Program.cs
@@ -12,7 +12,7 @@ namespace WriteHello
         static void Main()
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World! from the develop branch - Oct 4, 2018");
+            Console.WriteLine("Hello World! from the develop branch - Oct 4, 2018 - 10:26AM");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
